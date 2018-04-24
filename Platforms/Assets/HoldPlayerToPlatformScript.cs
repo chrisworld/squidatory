0fe9150d309c404222772e8d5d7a9eda0c09d694
Craig Smith <w1547319@my.westminster.ac.uk>
2018-04-25 00:34:03 +0100

    add platforms base script

---
+++ Platforms/Assets/HoldPlayerToPlatformScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoldPlayerToPlatformScript : MonoBehaviour {
+
+	void OnTriggerEnter(Collider col){
+		//Make moving platform parent of game object when game object collides with platform
+		col.transform.parent = gameObject.transform;
+	}
+
+	void OnTriggerExit(Collider col){
+		//Detaches game object from platform when game object exits platform
+		col.transform.parent = null;
+	}
+}
---
+++ Platforms/Assets/MovingPlatformScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatformScript : MonoBehaviour {
+
+	/* Platfrom positioning variables */
+	public Transform movingPlatform; // Platform current position
+	public Transform position1; // Start positon of platform
+	public Transform position2; // End positon of platform
+
+	/* Platform movement variables */
+	public Vector3 newPosition; // Moves platform
+	public string currentDirection; // Holds current direction of platform
+	public float smooth; // Controls smooth movement
+	public float resetTime; // Controls speed
+
+
+	// Use this for initialization
+	void Start () {
+		/* Set inital target for platform to move towards */
+		ChangeTarget ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		/* Moves platform from current position to new position over set time */
+		movingPlatform.position = Vector3.Lerp (movingPlatform.position, newPosition, smooth * Time.deltaTime);
+	}
+
+	/* Change direction of platform when reched end positions */
+	void ChangeTarget (){
+		/* Check for current direction and revert */
+		if (currentDirection == "Towards Position 1"){
+			currentDirection = "Towards Position 2";
+			newPosition = position2.position;
+		} else if (currentDirection == "Towards Position 2"){
+			currentDirection = "Towards Position 1";
+			newPosition = position1.position;
+		} else if (currentDirection == ""){ //Sets a direction if not initalised
+			currentDirection = "Towards Position 2";
+			newPosition = position2.position;
+		} 
+		Invoke ("ChangeTarget", resetTime);
+	}
+}

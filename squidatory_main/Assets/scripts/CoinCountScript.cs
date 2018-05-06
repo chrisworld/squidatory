eec51d1b11f6941d3e4e0c8f72b4600af65fb625
Craig Smith <w1547319@my.westminster.ac.uk>
2018-05-06 19:43:14 +0100

    created basic coin collection scene

---
+++ squidatory_main/Assets/scripts/CoinCountScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinCountScript : MonoBehaviour {
+	//Sets variable for coin counting
+	public int coins = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnGUI(){
+		//Show number of coins collected on GUI
+		GUI.Label (new Rect (10, 10, 150, 50), "Coins: " + coins);
+	}
+}
---
+++ squidatory_main/Assets/scripts/CoinScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter(Collider other){
+		//Trigger when player object collides with coin
+		if (other.name == "ThirdPersonController") {
+			Destroy (gameObject); //Remove coin game object from scene
+			other.GetComponent<CoinCountScript>().coins++; //Increment coin variable in coin count script
+		}
+	}
+}

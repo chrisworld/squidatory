a649613da1c8dd7e2b842b869545c917e557c880
Sarah Frank <sarah.frank@student.tugraz.at>
2018-05-13 14:49:16 +0200

    added maze door logic and basic scene

---
+++ squidatory_main/Assets/scripts/MazeScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int completedLvls = GameManager.instance_.getFLvlCount ();
+		//int completedLvls = 1;
+
+		for (int i = 1; i <= completedLvls; i++) {
+			string door = "MazeDoor" + i.ToString ();
+			GameObject obj = GameObject.Find (door);
+			obj.SetActive(false);
+		} 
+	}
+}

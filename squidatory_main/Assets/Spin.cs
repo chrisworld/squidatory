f7852872d2ac3aa7511307bcb9db7a1a27d9d91f
Craig Smith <w1547319@my.westminster.ac.uk>
2018-05-22 18:56:47 +0100

    Add spin to coins

---
+++ squidatory_main/Assets/Spin.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour {
+
+	//Speed of rotation
+	public float speed = 60f;
+	
+	// Update is called once per frame
+	void Update () {
+		//Rotates object anti-clockwise on Y-axis on each update
+		transform.Rotate(Vector3.left, speed * Time.deltaTime);
+	}
+}
--- squidatory_main/Assets/scripts/CoinScript.cs
+++ squidatory_main/Assets/scripts/CoinScript.cs
@@ -6,6 +6,7 @@ public class CoinScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+
 		
 	}
 	

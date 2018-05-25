d573d3f1dfca5430bd6d3f896c023bcbddc0174e
Sarah Frank <sarah.frank@student.tugraz.at>
2018-05-25 20:17:08 +0200

    fixed crouching problem in maze by changing the view

--- squidatory_main/Assets/scripts/MazeScript.cs
+++ squidatory_main/Assets/scripts/MazeScript.cs
@@ -7,12 +7,14 @@ public class MazeScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		int completedLvls = GameManager.instance_.getFLvlCount ();
-		//int completedLvls = 1;
+
+		// For testing purposes:
+		// int completedLvls = 1;
 
 		for (int i = 1; i <= completedLvls; i++) {
 			string door = "MazeDoor" + i.ToString ();
 			GameObject obj = GameObject.Find (door);
-			obj.SetActive(false);
+			DestroyObject (obj);
 		} 
 	}
 }

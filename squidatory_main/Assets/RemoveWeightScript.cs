938c7e43d09777a326499932b22b0a025f775b14
Craig Smith <w1547319@my.westminster.ac.uk>
2018-05-26 20:18:33 +0100

    Added weights to balance scene

---
+++ squidatory_main/Assets/RemoveWeightScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveWeightScript : MonoBehaviour {
+
+	//Runs when object enters box collider on ground
+	void OnTriggerEnter (Collider other){
+		//Checks if gameobject is weight
+		if (other.name == "LightWeight" ||
+			other.name == "MediumWeight" || 
+			other.name == "HeavyWeight")
+		{
+		//Destroys weight gameobject
+		Destroy (other.gameObject);
+		}
+	}
+}

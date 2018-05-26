4e91618f687d00dc7bc9373f9cb80de63ba1ec18
Craig Smith <w1547319@my.westminster.ac.uk>
2018-05-26 21:15:43 +0100

    Added weight UI buttons to balance scene

---
+++ squidatory_main/Assets/scripts/AddWeightScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddWeightScript : MonoBehaviour {
+
+	public void AddWeight () {
+		//Add new weight to scene
+		Instantiate (this);
+	}
+}

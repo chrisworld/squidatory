27f1d2d95bad37678be2f864a512801ff675c5ed
Craig Smith <w1547319@my.westminster.ac.uk>
2018-05-29 21:32:28 +0100

    Added UI panel to coins scene

---
+++ squidatory_main/Assets/MerchantCanvasScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MerchantCanvasScript : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		//Hide canvas on inital load
+		this.gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var test = GameObject.Find ("ThirdPersonController").GetComponent<CoinCountScript>();
+		var coins = test.coins;
+		Debug.Log ("coins is: " + coins);
+	}
+}
---
+++ squidatory_main/Assets/scripts/MerchantColliderScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MerchantColliderScript : MonoBehaviour {
+
+	public GameObject canvas;
+
+	void OnTriggerEnter (){
+		//Show canvas when player enters collider
+		canvas.SetActive(true);
+
+		//Get number of coins collected value
+		var CoinCount = GameObject.Find ("ThirdPersonController").GetComponent<CoinCountScript>();
+		var coins = CoinCount.coins;
+
+		if (coins < 5) {
+			Debug.Log ("You need more coins");
+		} else {
+			Debug.Log ("Great!");
+		}
+	
+	}
+		
+	void OnTriggerExit (){
+		//Hide canvas when player enters collider
+		canvas.SetActive(false);
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

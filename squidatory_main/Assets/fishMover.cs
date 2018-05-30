53b19bc798987c0325117a3ae552337797b5a965
mattyMattMatt <matt.hollick@gmail.com>
2018-05-30 08:59:15 +0100

    Graphic and Menu Update
    New Design Elements added to the design elements folder
    including All of the deadly sins symbols
    Main Menu now quits on click, some animation with some fixes to scaling

---
+++ squidatory_main/Assets/fishMover.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fishMover : MonoBehaviour {
+    private bool dirRight = true;
+    public float speed = 4.0f;
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (dirRight)
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        else
+            transform.Translate(-Vector2.right * speed * Time.deltaTime);
+
+        if (transform.position.x >= 4.0f)
+        {
+            dirRight = false;
+        }
+
+        if (transform.position.x <= -4)
+        {
+            dirRight = true;
+        }
+        //transform.Translate(0, 0, Time.deltaTime);
+        //transform.Translate(Time.deltaTime * 10, 0, 0, Space.World);
+    }
+}

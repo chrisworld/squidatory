efa08ace8905ef844de2c6304e4d844034874fc9
chrisworld <christian.walter@outlook.at>
2018-05-30 19:52:06 +0200

    badass music v1

--- squidatory_main/Assets/scripts/SoundManager.cs
+++ squidatory_main/Assets/scripts/SoundManager.cs
@@ -14,6 +14,10 @@ public class SoundManager : MonoBehaviour {
 
   // Array that holds all sounds in game
   public Sound[] sounds;
+  [HideInInspector]
+  public bool badass_activated;
+  [HideInInspector]
+  public bool badass_music_on;
 
   // Singelton instance
   public static SoundManager instance;
@@ -45,12 +49,28 @@ public class SoundManager : MonoBehaviour {
     //Play("jump"); 
     //Play("walking"); 
     //Play("cube_theme"); 
-    Play("squid_theme"); 
+    //Play("squid_theme"); 
+    //Play("button"); 
+    badass_activated = true;
+    badass_music_on = false;
   }
 
   // Update is called once per frame
   void Update () {
-  	
+  	if (badass_activated){
+      if (Input.GetKeyDown(KeyCode.M)){
+        if (!badass_music_on){
+          Stop("cube_theme");
+          Play("badass");
+          badass_music_on = true;
+        }
+        else {
+          Play("cube_theme");
+          Stop("badass");
+          badass_music_on = false;
+        }
+      }
+    }
   }
 
   // play specific Sound
@@ -71,5 +91,9 @@ public class SoundManager : MonoBehaviour {
       return;
     }
     s.source.Stop();
+  }
+
+  public void setBadass(){
+    badass_activated = true;
   }
 }

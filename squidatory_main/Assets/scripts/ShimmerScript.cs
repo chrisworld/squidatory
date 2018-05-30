b0db70fc95cc9b161c8715dc3ffd2eb0e3da59f5
Sarah Frank <sarah.frank@student.tugraz.at>
2018-05-30 18:45:29 +0100

    added shimmer scene + script

---
+++ squidatory_main/Assets/scripts/ShimmerScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShimmerScript : MonoBehaviour {
+
+	bool showText = false;
+
+	void OnCollisionEnter (Collision c) {
+		if (c.collider.name == "Character") {
+			showText = true;
+		}
+	}
+
+	void OnCollisionExit (Collision c) {
+		if (c.collider.name == "Character") {
+			showText = false;
+		}
+	}
+
+	void OnGUI () {
+		if (showText) {
+			GUI.Box (new Rect (30, 60, 300, 140), "It's so dark, I can't see. Please help me!");
+			if (GUI.Button (new Rect (40, 90, 270, 40), "Give away shimmer")) {
+				showText = false;
+			}
+			else if (GUI.Button (new Rect (40, 140, 270, 40), "Keep shimmer")) {
+				showText = false;
+			}
+
+		}
+	}
+}

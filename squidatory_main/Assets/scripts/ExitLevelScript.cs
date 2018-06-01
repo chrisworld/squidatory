d7d7d1c9436b8fa4455c3b821318995df46c2ceb
Craig Smith <w1547319@my.westminster.ac.uk>
2018-06-01 09:07:08 +0100

    Exit buttons

---
+++ squidatory_main/Assets/scripts/ExitLevelScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitLevelScript : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("COL");
+        //Return back to 3D world
+        SceneManager.LoadScene("OuterWorld");
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

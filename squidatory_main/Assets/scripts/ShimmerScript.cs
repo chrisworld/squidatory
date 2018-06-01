42550db2ea8c09d5affd038fbd20193c359a46ec
Craig Smith <w1547319@my.westminster.ac.uk>
2018-06-01 09:54:33 +0100

    FINISHED GAME!!

--- squidatory_main/Assets/scripts/ShimmerScript.cs
+++ squidatory_main/Assets/scripts/ShimmerScript.cs
@@ -22,13 +22,24 @@ public class ShimmerScript : MonoBehaviour {
 	void OnGUI () {
 		if (showText) {
 			GUI.Box (new Rect (30, 60, 295, 140), "It's so dark, I can't see. Please help me!");
-			if (GUI.Button (new Rect (40, 90, 270, 40), "Give away shimmer")) {
+            if (GUI.Button (new Rect (40, 90, 270, 40), "Give away shimmer")) {
 				showText = false;
+                good();
 			}
 			else if (GUI.Button (new Rect (40, 140, 270, 40), "Keep shimmer")) {
 				showText = false;
+                bad();
 			}
 
 		}
 	}
+
+    void good(){
+        GameObject.Find("2D_Box_White2").transform.Translate(0, 10, 0);
+    }
+
+    void bad()
+    {
+        GameObject.Find("2D_Box_Black2").transform.Translate(0, 10, 0);
+    }
 }
